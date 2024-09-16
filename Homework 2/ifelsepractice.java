dfc3dbce05f6fff73cb55660b076ce2decb85f00
shgarcia23 <73551222+shgarcia23@users.noreply.github.com>
2024-09-15 23:14:55 -0400

    Update ifelsepractice.java

--- Homework 2/ifelsepractice.java
+++ Homework 2/ifelsepractice.java
@@ -8,12 +8,64 @@ import java.util.Scanner;
 * can be any topic you pick :) feel free to pick some obscure or niche topics!
 * good luck!
 * */
-public class Main {
-    public static void main(String[] args) {
+public class Main {public static void main(String[] args) {
 
+    Scanner sc = new Scanner(System.in);
 
+    System.out.println("which animal are you ");
+    System.out.println("How old are you?");
+    int age=sc.nextInt();
 
+    System.out.println("whats your favorite color between (red,blue,yellow)");
+    sc.nextLine();
+    String color=sc.nextLine();
+
+    System.out.println("Male or female");
+    String gender=sc.nextLine();
+
+    if(age >= 20 && color.equals("red") && gender.equals("male") ){
+        System.out.println("rat");
     }
+    else if (age<20 && color.equals("red") && gender.equals("male")){
+        System.out.println("cat");
+    }
+    else if (age>20 && color.equals("red")&& gender.equals("female")){
+        System.out.println("dog");
+    }
+    else if (age<20 && color.equals("red")&& gender.equals("female")){
+        System.out.println("rat");
+    }
+    else if(age>20 && color.equals("blue") && gender.equals("male") ){
+        System.out.println("rat");
+    }
+    else if (age<20 && color.equals("blue") && gender.equals("male")){
+        System.out.println("cat");
+    }
+    else if (age>20 && color.equals("blue")&& gender.equals("female")){
+        System.out.println("dog");
+    }
+    else if (age<20 && color.equals("blue")&& gender.equals("female")){
+        System.out.println("rat");
+    }
+    else if(age>20 && color.equals("yellow") && gender.equals("male") ){
+        System.out.println("rat");
+    }
+    else if (age<20 && color.equals("yellow") && gender.equals("male")){
+        System.out.println("cat");
+    }
+    else if (age>20 && color.equals("yellow")&& gender.equals("female")){
+        System.out.println("dog");
+    }
+    else if (age<20 && color.equals("yellow")&& gender.equals("female")){
+        System.out.println("rat");
+    }
+
+
+
+
+    
+    else {System.out.println("not a combo");}
+}
 }
 
 
