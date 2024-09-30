3f7f64dbe8667d0464f3d1e37b9e6159d4aa08c6
shgarcia23 <73551222+shgarcia23@users.noreply.github.com>
2024-09-29 22:26:15 -0400

    Update Loops.java

--- Homework 3/Loops.java
+++ Homework 3/Loops.java
@@ -8,11 +8,20 @@ how did you figure out how to do this? can you translate your code into simple t
 you need to explain why you picked your for loop conditional and what's doing the work with the string
 if you're confused reach out!
  */
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Enter A word or name");
+        Scanner sc=new Scanner(System.in);
+        String userinput=sc.nextLine();
+        for(int i =0 ;i<userinput.length();i++){
+               
+            System.out.println(userinput.charAt(i));  
+    
+            }
 
-
-
+        }
     }
-}
+///since we had did the last problem in class i knew the the .
+//length was the way to get the idex of the string so i kept on
+// trying different things until i was able to get my output  
