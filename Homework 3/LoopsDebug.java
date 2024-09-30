ca63447fb202d77b648a45fbb7790f1f15c7bf23
shgarcia23 <73551222+shgarcia23@users.noreply.github.com>
2024-09-29 22:27:58 -0400

    Update LoopsDebug.java

--- Homework 3/LoopsDebug.java
+++ Homework 3/LoopsDebug.java
@@ -1,11 +1,11 @@
-
-public class Main {
+import java.util.Scanner;
+public class Loops {
     public static void main(String[] args) {
 
 
         //P1: This one only prints 0-9, can you fix it so it prints 1-10?
         System.out.println("Problem 1");
-        for (int i = 0; i < 10; i++){
+        for (int i = 0; i <= 10; i++){
             System.out.println(i);
         }
 
@@ -13,14 +13,25 @@ public class Main {
         //(factorial = X!, X being the user input, Factorials are every digit before X multiplied together)
         System.out.println("Problem 2");
         System.out.println("Enter a number and I will tell you the fatorial: ");
+        Scanner sc=new Scanner(System.in);
+        int user_num =sc.nextInt();
+        int sum=1;
         //here's a hint
-        for (int i = 0; i < ?; i++){
+        for (int i = 1; i <= user_num; i++){
+        sum *=i;
 
         }
+        System.out.println(sum);
 
         //P3: Ask the user for a number, and then add together every OTHER digit (starting from 1)
         System.out.println("Problem 3");
         System.out.println("Enter a number and I will tell you the sum of every other number: ");
+        int usernumONe_=sc.nextInt();
+        int sumone=0;
+        for (int i = 1; i <= usernumONe_; i+=2){
+            sumone=sumone+i;
+        }
+        System.out.println(sumone);
         //No hint! what do you need to complete this task?
 
 
@@ -29,13 +40,29 @@ public class Main {
         System.out.println("Problem 4");
         boolean run = true;
         while (run == true){
-            System.out.println("I printed once!");
+            run=false;
+          System.out.println(
+          "I printed once!");
         }
 
         //P5: Take a string from the user and print them the reverse!
-        System.out.println("Problem 5");
+       System.out.println("Problem 5");
+       String userinput=sc.nextLine();
+       char ch;
+
         //hint
         String reverse = "";
+        userinput=sc.nextLine();
+        for(int i =0 ;i<userinput.length();i++){
+            ch=userinput.charAt(i);
+            reverse=ch+reverse;
+
+        }System.out.println("reverse input"+"="+reverse);
+
+    }
+}
+
+       
 
     }
 }
