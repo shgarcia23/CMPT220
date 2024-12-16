ed277e8c1cea93943d471c5c3ba2f4e1285fce54
shgarcia23 <73551222+shgarcia23@users.noreply.github.com>
2024-12-16 17:41:41 -0500

    Update ArrayDebug.java

--- Homework 4/ArrayDebug.java
+++ Homework 4/ArrayDebug.java
@@ -3,11 +3,14 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
-        //Create me an array named "nameArray" that is a string and has 4 values in it, give me 4 names of people or animals you care about
+            //Create me an array named "nameArray" that is a string and has 4 values in it, give me 4 names of people or animals you care about
 
         //Create me an array without any values, but it's an integer array named "numArray"
 
         //Create me an int array named "newArray" and set it to have 20 null spaces
+        String[] nameArray ={"Brayan","jin jin","marco","quetzaly"};
+        Integer[] numArray={};
+        int[] newArray=new int[20];
 
 
 
@@ -16,9 +19,9 @@ public class Main {
         //Here's our first practice array!
         String[] sortMe = {"Apple","Kiwi","Orange","Banana","Watermelon","Strawberry"};
        //What method is missing here to actually sort our array?
-        Arrays.?(sortMe);
+        Arrays.sort(sortMe);
         //And what method is missing here to print out that array?
-        System.out.println(?);
+        System.out.println(Arrays.toString(sortMe));
         //After this, you cannot use the array helper file, gotta go back to the old ways!!
 
         //another one!
@@ -26,14 +29,21 @@ public class Main {
         //How would we add every index together and print out the sum??
         //A small hint:
         int sum = 0;
-        for(?;?;?){
-
+        for(int i = 0; i < addMe.length; i++) {
+            sum += addMe[i];
         }
-        
+        System.out.println(sum);
+    
         //thinking one! here's an array
         int maxMe[] = {280, 4892, 134, 23, 1937, 5329, 932, 89};
         //What would you have to do to find the largest number and then print that value?
-        
+        int max =maxMe[0];
+        for(int i =1<max.length;i++){
+            if (maxMe[i]>max{
+                max=maxMe[i];
+            }
+        }
+        System.out.println(max)
 
     }
 }
