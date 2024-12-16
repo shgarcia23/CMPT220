50129d57cf8499ee29978688bdeababf8610e995
shgarcia23 <73551222+shgarcia23@users.noreply.github.com>
2024-12-16 17:27:45 -0500

    Update Array.java

--- Homework 4/Array.java
+++ Homework 4/Array.java
@@ -7,10 +7,25 @@ Take the given array and keep track of:
 
  */
 
-public class Main {
+public class Array {
     public static void main(String[] args) {
         //Don't let the size of this scare you! no matter how big an array is, it all works the same!
         int[] myArray = {10,3,295,38,20,3,4,267,2445,10,5566,87,93,17,10,2,87,267,3176,3,82};
         //you cannot use the array util. Do this one by hand :(
+        for(int i =0 ;i < myArray.length;i++){
+            if (myArray[i]!=-1){
+             int count = 1;   
+            
+             for(int x=i+1;x<myArray.length;x++){
+                if(myArray[i]==myArray[x]){
+                count++;
+                myArray[x]=-1;
+            }
+        }
+            if (count!=1&&myArray[i]!=0) {
+                System.out.println(myArray[i] + " appears " + count + " times.");
+            }
+        }
     }
+}
 }
